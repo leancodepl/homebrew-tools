c72724336fd24cdd1f215c1f9ac19845e9907164
Bartek Pacia <barpac02@gmail.com>
2021-10-24 14:11:24 +0200

    bump formula version

--- Formula/mobile-tools.rb
+++ Formula/mobile-tools.rb
@@ -2,7 +2,7 @@ class MobileTools < Formula
   desc "Useful program for mobile development."
   homepage "https://github.com/leancodepl/mobile-tools"
   url "https://github.com/leancodepl/mobile-tools.git", :branch => 'master'
-  version "0.2.0"
+  version "0.2.1"
 
   depends_on "jq"
 
