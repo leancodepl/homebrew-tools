cd4b0ca488f940e657fa06c11a6d99b9d4427b63
Bartek Pacia <barpac02@gmail.com>
2021-10-24 14:51:33 +0200

    follow Homebrew recommendations
    * fix all warnings raised by `brew audit mobile-tools --strict`
    
    * add a simple test block

--- Formula/mobile-tools.rb
+++ Formula/mobile-tools.rb
@@ -1,8 +1,10 @@
 class MobileTools < Formula
-  desc "Small but useful program for mobile development"
+  desc "Useful programs for mobile development"
   homepage "https://github.com/leancodepl/mobile-tools"
-  url "https://github.com/leancodepl/mobile-tools.git", :branch => 'master'
+  url "https://github.com/leancodepl/mobile-tools.git", branch: "master"
   version "1.2.0"
+
+  depends_on "jq"
 
   def install
     bin.install "bin/bs_android"
@@ -11,5 +13,10 @@ class MobileTools < Formula
     bin.install "bin/generate_keystore"
     bin.install "bin/is_prerelease"
     bin.install "bin/link_changelog"
+  end
+
+  test do
+    system "command", "-v", "generate-keystores"
+    system "command", "-v", "poeditor-download"
   end
 end
