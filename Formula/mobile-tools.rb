aa621f23cecd781637188de0d54922a6b9cd122a
Bartek Pacia <bartek.pacia@leancode.pl>
2023-02-20 12:53:58 +0100

    remove poeditor-download

--- Formula/mobile-tools.rb
+++ Formula/mobile-tools.rb
@@ -2,7 +2,7 @@ class MobileTools < Formula
   desc "Useful program for mobile development."
   homepage "https://github.com/leancodepl/mobile-tools"
   url "https://github.com/leancodepl/mobile-tools.git", :branch => 'master'
-  version "0.2.1"
+  version "0.2.2"
 
   depends_on "jq"
 
@@ -11,6 +11,5 @@ class MobileTools < Formula
     bin.install "bin/generate_keystore"
     bin.install "bin/is_prerelease"
     bin.install "bin/link_changelog"
-    bin.install "bin/poeditor_download"
   end
 end
