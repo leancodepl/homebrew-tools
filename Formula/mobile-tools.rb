2527935cba351c2dfb824c9e516c6a14bae6a8e8
Bartek Pacia <bartek.pacia@leancode.pl>
2023-07-14 00:37:58 +0200

    add `bs_android` script to the `mobile-tools` formula

--- Formula/mobile-tools.rb
+++ Formula/mobile-tools.rb
@@ -2,9 +2,10 @@ class MobileTools < Formula
   desc "Small but useful program for mobile development"
   homepage "https://github.com/leancodepl/mobile-tools"
   url "https://github.com/leancodepl/mobile-tools.git", :branch => 'master'
-  version "1.0.0"
+  version "1.1.0"
 
   def install
+    bin.install "bin/bs_android"
     bin.install "bin/echo_changelog"
     bin.install "bin/generate_keystore"
     bin.install "bin/is_prerelease"
