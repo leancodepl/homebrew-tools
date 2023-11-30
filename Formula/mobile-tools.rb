536162024d9d921c437615cc4094dc5b7a348ebf
Bartek Pacia <bartek.pacia@leancode.pl>
2023-11-30 12:15:14 +0100

    add bs_ios

--- Formula/mobile-tools.rb
+++ Formula/mobile-tools.rb
@@ -2,10 +2,11 @@ class MobileTools < Formula
   desc "Small but useful program for mobile development"
   homepage "https://github.com/leancodepl/mobile-tools"
   url "https://github.com/leancodepl/mobile-tools.git", :branch => 'master'
-  version "1.1.0"
+  version "1.2.0"
 
   def install
     bin.install "bin/bs_android"
+    bin.install "bin/bs_ios"
     bin.install "bin/echo_changelog"
     bin.install "bin/generate_keystore"
     bin.install "bin/is_prerelease"
