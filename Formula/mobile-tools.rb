eed0cbac51d29976f4d3b382f63fa903f199290e
Bartek Pacia <barpac02@gmail.com>
2023-12-01 20:47:56 +0100

    enable `brew test`

--- Formula/mobile-tools.rb
+++ Formula/mobile-tools.rb
@@ -16,7 +16,21 @@ class MobileTools < Formula
   end
 
   test do
-    system "command", "-v", "generate-keystores"
-    system "command", "-v", "poeditor-download"
+    system "command", "-v", "generate_keystore"
+
+    assert_match(
+      "true",
+      shell_output(bin/"is_prerelease maestro-v0.6.9"),
+    )
+
+    assert_match(
+      "false",
+      shell_output(bin/"is_prerelease maestro-v1.2.3"),
+    )
+
+    assert_match(
+      "https://pub.dev/packages/maestro/changelog#4203",
+      shell_output(bin/"link_changelog maestro v4.2.0+3"),
+    )
   end
 end
