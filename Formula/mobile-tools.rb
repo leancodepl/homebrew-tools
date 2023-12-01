e664c9f3245489bfcfe5f1d6c01ac534c39e568f
Bartek Pacia <bartek.pacia@leancode.pl>
2023-12-01 21:32:18 +0100

    Add GitHub Action to enforce good formula style (#6)

--- Formula/mobile-tools.rb
+++ Formula/mobile-tools.rb
@@ -1,8 +1,10 @@
 class MobileTools < Formula
-  desc "Small but useful program for mobile development"
+  desc "Useful programs for mobile development"
   homepage "https://github.com/leancodepl/mobile-tools"
-  url "https://github.com/leancodepl/mobile-tools.git", :branch => 'master'
+  url "https://github.com/leancodepl/mobile-tools.git", branch: "master"
   version "1.2.0"
+
+  depends_on "jq"
 
   def install
     bin.install "bin/bs_android"
@@ -11,5 +13,24 @@ class MobileTools < Formula
     bin.install "bin/generate_keystore"
     bin.install "bin/is_prerelease"
     bin.install "bin/link_changelog"
+  end
+
+  test do
+    system "command", "-v", "generate_keystore"
+
+    assert_match(
+      "true",
+      shell_output(bin/"is_prerelease maestro-v0.6.9"),
+    )
+
+    assert_match(
+      "false",
+      shell_output(bin/"is_prerelease maestro-v1.2.3"),
+    )
+
+    assert_match(
+      "https://pub.dev/packages/maestro/changelog#4203",
+      shell_output(bin/"link_changelog maestro v4.2.0+3"),
+    )
   end
 end
